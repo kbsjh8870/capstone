59a7598a91bc13d2b1cc9c1ff4ec578bbdc241bc
KJH <kjhkmn80@gmail.com>
2025-04-17 19:42:42 +0900

    bt

--- Backend/src/main/java/com/example/Backend/BackendApplication.java
+++ Backend/src/main/java/com/example/Backend/BackendApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-//branchTest2222
+
+	// TTTTTTEEEEEESSSSSTTTTTT
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
