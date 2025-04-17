13dda138ad2b6e825c3898d19fedd00a16536515
KJH <68000689+kbsjh8870@users.noreply.github.com>
2025-04-17 22:03:13 +0900

    Update start.sh

--- Backend/scripts/start.sh
+++ Backend/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/.ssh/capstone/Backend"
+PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/capstone.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
